a620e95a3462af4597d98294172f5d915bca5820
jimvon <JimVon@china.liantong.com>
2016-06-27 20:52:17 +0800

    modify commentsCount stuffs

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -7,8 +7,5 @@ Template.postItem.helpers({
         a.href = this.url;
         console.log('post_item: { href: ' + a.href + ', hostname: ' + a.hostname + '}');
         return a.hostname;
-    },
-    commentsCount: function() {
-        return Comments.find({ postId: this._id }).count();
     }
 });
