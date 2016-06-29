0acd0248757ccd8a22db844ec5f51b2fdb5c7d63
jimvon <JimVon@china.liantong.com>
2016-06-29 15:02:16 +0800

    add page-partation stuffs

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,9 @@
-Meteor.publish('posts', function() {
-    //return Posts.find({flagged: false}); 
-    return Posts.find();
+Meteor.publish('posts', function(options) {
+check(options, {
+    sort: Object,
+    limit: Number
+  });
+  return Posts.find({}, {sort: options.sort, limit: options.limit});
 });
 
 Meteor.publish('comments', function(postId) {
